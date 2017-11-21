6d57c84a0c3f5c2eae0fe0d77ac5f1f2214222a5
Youngbok Yoon <master@bluehack.net>
2017-11-21 12:08:17 +0900

    [BL-149][WIP] Comlemented implementaion for basic function

---
+++ grpc/vector_search.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+from __future__ import absolute_import
+
+import numpy as np
+import os
+import faiss
+from bluelens_log import Logging
+
+INDEX_FILE = os.environ['INDEX_FILE']
+REDIS_SERVER = os.environ['REDIS_SERVER']
+REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
+options = {
+  'REDIS_SERVER': REDIS_SERVER,
+  'REDIS_PASSWORD': REDIS_PASSWORD
+}
+log = Logging(options, tag='bl-search-vector')
+
+class SearchVector(object):
+  def __init__(self):
+    try:
+      self.index = faiss.read_index(INDEX_FILE)
+    except Exception as e:
+      log.error(str(e))
+
+  def search(self, vector):
+    xq = np.expand_dims(np.array(vector, dtype=np.float32), axis=0)
+
+    xq.astype(np.float32)
+    result_d, result_i = self.index.search(xq, 10)
+    log.debug(result_i)
+    log.debug(result_d)
+    log.debug(result_i)
+    return np.squeeze(result_i)
+
+
---
+++ grpc/vector_search_server.py
@@ -0,0 +1,54 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from concurrent import futures
+import time
+
+import grpc
+import os
+
+from vector_search import SearchVector
+import vector_search_pb2
+import vector_search_pb2_grpc
+
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class Search(vector_search_pb2_grpc.SearchServicer):
+  def __init__(self):
+    self.vc = SearchVector()
+
+  def SearchVector(self, request, context):
+    results = self.vc.search(request.vector)
+
+    for result in results:
+      searchReply = vector_search_pb2.SearchReply()
+      searchReply.vector = result
+      yield searchReply
+
+
+def serve():
+  server = grpc.server(futures.ThreadPoolExecutor(max_workers=50))
+  vector_search_pb2_grpc.add_SearchServicer_to_server(Search(), server)
+  server.add_insecure_port('[::]:50054')
+  server.start()
+  try:
+    while True:
+      time.sleep(_ONE_DAY_IN_SECONDS)
+  except KeyboardInterrupt:
+    server.stop(0)
+
+if __name__ == '__main__':
+  serve()
