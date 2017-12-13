6e5745035a4ae7daaa3d76de47e1f0d64b5e1d2d
Youngbok Yoon <master@bluehack.net>
2017-12-13 11:34:35 +0900

    Changed tag to 'dev'

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 export ORG="bluelens"
 export IMAGE="bl-search-vector"
-export TAG="latest"
+export TAG="dev"
 
 docker login
 
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo nvidia-docker run -dit --restart unless-stopped -p 50054:50054 bluelens/bl-search-vector:latest
+sudo nvidia-docker run -dit --restart unless-stopped -p 50054:50054 bluelens/bl-search-vector:dev
